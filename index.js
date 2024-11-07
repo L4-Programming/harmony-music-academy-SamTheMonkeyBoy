55d4ee80976be648b6888fba6b220e1982b10662
SamTheMonkeyBoy <sam.dlt@highlands.ac.uk>
2024-11-07 11:09:00 +0000

    Comment Plan

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 // Include input fields for the user's email address, level, and hours of study
 // Capture user's input on form submission
 // Get the user's email address - userEmail (string)
-// Get the user's level - userLevel ()
-// Get the user's hours of study studyHours ()
+// Get the user's level - userLevel (string)
+// Get the user's hours of study userHours (number)
 // Validate the user's input
-// Check if the user has selected a level
-// Check if the user has provided an email address
-// Check if the user has specified at least one hour of study
-// Check if the number of hours requested is within the allowed range
+// Check if the user has selected a level - check level entered; check selected "Beginner" or "Advanced"; conditional statements
+// Check if the user has provided an email address - check email entered, valid format (e.g., name@domain.com).
+// Check if the user has specified at least one hour of study - must be => 1
+// Check if the number of hours requested is within the allowed range - <= to mx number allowed in level
 // Calculate the total cost
 // Display the total cost to the user
