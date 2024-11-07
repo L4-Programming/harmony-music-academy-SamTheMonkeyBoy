24aa70ce1d5a7657b7ea22ed236cb12d4854f79a
SamTheMonkeyBoy <sam.dlt@highlands.ac.uk>
2024-11-07 10:31:54 +0000

    Logo above on mobile

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-// javascript
-
-console.log("Hello there. General Kenobi!");
+// Display a form to the user
+// Include input fields for the user's email address, level, and hours of study
+// Capture user's input on form submission
+// Get the user's email address - userEmail (string)
+// Get the user's level - userLevel ()
+// Get the user's hours of study studyHours ()
+// Validate the user's input
+// Check if the user has selected a level
+// Check if the user has provided an email address
+// Check if the user has specified at least one hour of study
+// Check if the number of hours requested is within the allowed range
+// Calculate the total cost
+// Display the total cost to the user
